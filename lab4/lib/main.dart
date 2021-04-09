c0a22fbfed62d999e9a38e05d82c73cd77b28a56
Ivan Burlakov <burlakovv.ivan@gmail.com>
2021-04-09 14:05:33 +0300

    add lab4

---
+++ lab4/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'Home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Ivan Burlakov - Lab 4',
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color(0xFFFFFFFF),
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Home(title: 'Ivan Burlakov - Lab 4'),
+    );
+  }
+}
